f5c3cd6c3aca0b62c3ffe95df08f3ddaeb5b479d
061103 <109753786@qq.com>
2019-03-22 11:56:59 +0800

    a

---
+++ app/src/main/java/com/zhou/biyongxposed/OneHook.java
@@ -0,0 +1,94 @@
+package com.zhou.biyongxposed;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+
+public class OneHook implements IXposedHookLoadPackage {
+    static String strClassName = "";
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("oneapp.onechain.androidapp")) {
+            XposedBridge.log("Loaded App:" + lpparam.packageName);
+            XposedHelpers.findAndHookMethod(ClassLoader.class,"loadClass",String.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    // 获取指定名称的类加载之后的Class<?>
+                    Class<?> clazz = (Class<?>) param.getResult();
+                    // 获取加载的指定类的名称
+                    String strClazz = clazz.getName();
+                    XposedBridge.log("LoadClass : " + strClazz);
+                    // 所有的类都是通过loadClass方法加载的
+                    // 获取被Hook的目标类的名称
+                            strClassName = strClazz;
+                            //XposedBridge.log("HookedClass : "+strClazz);
+                            // 获取到指定名称类声明的所有方法的信息
+                            Method[] m = clazz.getDeclaredMethods();
+                            // 打印获取到的所有的类方法的信息
+                            for (int i = 0; i < m.length; i++) {
+
+                                //XposedBridge.log("HOOKED CLASS-METHOD: "+strClazz+"-"+m[i].toString());
+                                if (!Modifier.isAbstract(m[i].getModifiers())           // 过滤掉指定名称类中声明的抽象方法
+                                        && !Modifier.isNative(m[i].getModifiers())     // 过滤掉指定名称类中声明的Native方法
+                                        && !Modifier.isInterface(m[i].getModifiers())  // 过滤掉指定名称类中声明的接口方法
+                                        ) {
+
+                                    // 对指定名称类中声明的非抽象方法进行java Hook处理
+                                    XposedBridge.hookMethod(m[i], new XC_MethodHook() {
+
+                                        // 被java Hook的类方法执行完毕之后，打印log日志
+                                        @Override
+                                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+
+                                            // 打印被java Hook的类方法的名称和参数类型等信息
+                                            XposedBridge.log("HOOKED METHOD: " + strClassName + "-" + param.method.toString());
+                                        }
+                                    });
+
+                                }
+                            }
+                        }
+                    });
+        }
+    }
+    // 获取指定名称的类声明的类成员变量、类方法、内部类的信息
+    public void dumpClass(Class<?> actions) {
+
+        XposedBridge.log("Dump class " + actions.getName());
+        XposedBridge.log("Methods");
+
+        // 获取到指定名称类声明的所有方法的信息
+        Method[] m = actions.getDeclaredMethods();
+        // 打印获取到的所有的类方法的信息
+        for (int i = 0; i < m.length; i++) {
+
+            XposedBridge.log(m[i].toString());
+        }
+
+        XposedBridge.log("Fields");
+        // 获取到指定名称类声明的所有变量的信息
+        Field[] f = actions.getDeclaredFields();
+        // 打印获取到的所有变量的信息
+        for (int j = 0; j < f.length; j++) {
+
+            XposedBridge.log(f[j].toString());
+        }
+
+        XposedBridge.log("Classes");
+        // 获取到指定名称类中声明的所有内部类的信息
+        Class<?>[] c = actions.getDeclaredClasses();
+        // 打印获取到的所有内部类的信息
+        for (int k = 0; k < c.length; k++) {
+
+            XposedBridge.log(c[k].toString());
+        }
+    }
+}
