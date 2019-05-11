abf31186850203ea694295e4fcf17388be8caa08
061103 <109753786@qq.com>
2019-05-11 12:55:37 +0800

    基本可以运行,目前一个问题困扰我，辅助服务遍历红包时，只能遍历当前打开的activity，而之前发过的，不可见的，也没有点击过的红包，我不知道怎么去找出来（遍历出来）。

--- app/src/main/java/com/zhou/biyongxposed/MainActivity.java
+++ app/src/main/java/com/zhou/biyongxposed/MainActivity.java
@@ -1,20 +1,31 @@
 package com.zhou.biyongxposed;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import static android.provider.Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES;
+
 
 public class MainActivity extends AppCompatActivity {
-    private long lastBack = 0;
-
+    private boolean run = false;
+    private final Handler handler = new Handler();
+    long lastBack = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        run = true;
+        handler.postDelayed(task, 1000);
         Button shuoming=findViewById(R.id.button);
         shuoming.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +34,65 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private final Runnable task = new Runnable() {
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            if (run) {
+                Button serverstatus= findViewById(R.id.serverstatus);
+                if(isAccessibilitySettingsOn(MainActivity.this)){
+                    serverstatus.setText("开启");
+                    serverstatus.setTextColor(Color.parseColor("#33FF33"));
+                    serverstatus.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Settings.Secure.putString(getContentResolver(), ENABLED_ACCESSIBILITY_SERVICES, "com.zhou.biyongxposed/com.zhou.biyongxposed.bingyongserver" );
+                            Settings.Secure.putInt(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, 0);
+                        }
+                    });
+                }else {
+                    serverstatus.setText("关闭");
+                    serverstatus.setTextColor(Color.parseColor("#999999"));
+                    serverstatus.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Settings.Secure.putString(getContentResolver(), ENABLED_ACCESSIBILITY_SERVICES, "com.zhou.biyongxposed/com.zhou.biyongxposed.bingyongserver");
+                            Settings.Secure.putInt(getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED, 1);
+                        }
+                    });
+                }
+                handler.postDelayed(this, 1000);
+            }
+        }
+    };
+    private boolean isAccessibilitySettingsOn(Context mContext) {
+        int accessibilityEnabled = 0;
+        String accInfo = "com.zhou.biyongxposed/com.zhou.biyongxposed.bingyongserver";
+        final String service = accInfo;
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(mContext.getApplicationContext().getContentResolver(), android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            Log.e("biyong", "Error finding setting, default accessibility to not found: "
+                    + e.getMessage());
+        }
+        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
+        if (accessibilityEnabled == 1) {
+            String settingValue = Settings.Secure.getString(mContext.getApplicationContext().getContentResolver(), ENABLED_ACCESSIBILITY_SERVICES);
+            if (settingValue != null) {
+                mStringColonSplitter.setString(settingValue);
+                while (mStringColonSplitter.hasNext()) {
+                    String accessibilityService = mStringColonSplitter.next();
+                    if (accessibilityService.equalsIgnoreCase(service)) {
+                        Log.v("biyong", "We've found the correct setting - accessibility is switched on!");
+                        return true;
+                    }
+                }
+            }
+        }else {
+            Log.v("biyong", accInfo +"服务巳关闭");
+        }
+        return false;
     }
     /**
      * 再次返回键退出程序
