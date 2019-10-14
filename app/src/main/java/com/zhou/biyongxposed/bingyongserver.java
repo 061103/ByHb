9b6ded6a9bbb2b9a4b06acde354535bacc4799dd
061103 <109753786@qq.com>
2019-10-14 18:01:59 +0800

    BIYONG

--- app/src/main/java/com/zhou/biyongxposed/bingyongserver.java
+++ app/src/main/java/com/zhou/biyongxposed/bingyongserver.java
@@ -136,22 +136,24 @@ public class bingyongserver extends AccessibilityService {
                             slk = false;
                             List<AccessibilityNodeInfo> red_paket_status = rootNode.findAccessibilityNodeInfosByViewId("org.telegram.biyongx:id/cell_red_paket_status");
                             List<AccessibilityNodeInfo> red_paket_sender = rootNode.findAccessibilityNodeInfosByViewId("org.telegram.biyongx:id/cell_red_paket_sender");
-                            List<AccessibilityNodeInfo> red_paket_message = rootNode.findAccessibilityNodeInfosByViewId("org.telegram.biyongx:id/cell_red_paket_message");
                             findRedPacketSender = new AccessibilityNodeInfo[red_paket_status.size()];
                             if (!red_paket_status.isEmpty()) {
                                 sleepTime(findSleeper);//发现红包延时控制
                                 Log.i("Biyong", "发现红包");
                                 LogUtils.i("发现红包");
                                 for (int i = 0; i < red_paket_status.size(); i++) {
-                                    if (red_paket_status.get(i).getText().toString().equals("领取红包")&& !red_paket_message.get(i).getText().equals("答题红包")) {
-                                        Log.i("Biyong:", "找到第"+(i+1)+"个红包的关键字."+red_paket_status.get(i).getText());
-                                        LogUtils.i("找到第"+(i+1)+"个红包的关键字."+red_paket_status.get(i).getText());
-                                        have = true;
-                                        meizhaodao=true;
-                                        findRedPacketSender[i] = red_paket_sender.get(i);
-                                        Log.i("Biyong:", "第" + (i + 1) + "个红包类型为:" + findRedPacketSender[i].getText());
-                                        LogUtils.i("第" + (i + 1) + "个红包为:" + findRedPacketSender[i].getText());
-                                    } else if (!meizhaodao) {
+                                    if (red_paket_status.get(i).getText().toString().equals("领取红包")){
+                                        List<AccessibilityNodeInfo> red_paket_message = rootNode.findAccessibilityNodeInfosByViewId("org.telegram.biyongx:id/cell_red_paket_message");
+                                           if (!red_paket_message.isEmpty()&&!red_paket_message.get(i).getText().equals("答题红包")) {
+                                                Log.i("Biyong:", "找到第"+(i+1)+"个红包的关键字."+red_paket_status.get(i).getText());
+                                                LogUtils.i("找到第"+(i+1)+"个红包的关键字."+red_paket_status.get(i).getText());
+                                                have = true;
+                                                meizhaodao=true;
+                                                findRedPacketSender[i] = red_paket_sender.get(i);
+                                                Log.i("Biyong:", "第" + (i + 1) + "个红包类型为:" + findRedPacketSender[i].getText());
+                                                LogUtils.i("第" + (i + 1) + "个红包为:" + findRedPacketSender[i].getText());
+                                            }
+                                    }else if (!meizhaodao) {
                                             Log.i("Biyong:", "红包可能巳被拆开或领完或过期.");
                                             LogUtils.i("红包可能巳被拆开或领完或过期.");
                                             execShellCmd("input swipe 1057 2093 1153 652");
@@ -187,7 +189,7 @@ public class bingyongserver extends AccessibilityService {
                                     if(huadong==2){execShellCmd("input tap 1342 2284");
                                     Log.i("swipe:","滑动"+huadong+"次,没有找到红包，直接点击坐标！");
                                     LogUtils.i("滑动了"+huadong+"次,没有找到红包，直接点击坐标！");
-                                    sleepTime(1500);}
+                                    sleepTime(1800);}
                                     return;
                                 }
                             }
