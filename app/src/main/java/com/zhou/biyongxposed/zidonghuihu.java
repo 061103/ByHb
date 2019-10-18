e30adc00322c3af23e151e4d70fa9a38bb4a5127
061103 <109753786@qq.com>
2019-10-18 18:17:17 +0800

    BIYONG

--- app/src/main/java/com/zhou/biyongxposed/zidonghuihu.java
+++ app/src/main/java/com/zhou/biyongxposed/zidonghuihu.java
@@ -9,33 +9,29 @@ import android.widget.Toast;
 
 public class zidonghuihu extends AppCompatActivity {
     private Button zdhf;
-    private boolean hf;
+    private boolean zdhfmessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_huifu);
         zdhf = findViewById(R.id.zidonghuifubutton);
-        if(!hf){
-            zdhf.setText("回复关闭");
-            zdhf.setTextColor(Color.parseColor("#999999"));
-        }else{
-            zdhf.setText("服务开启");
-            zdhf.setTextColor(Color.parseColor("#33FF33"));
-        }
         zdhf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(zidonghuihu.this, "点击完成", Toast.LENGTH_SHORT).show();
-                if(!hf) {
-                    hf=true;
-                    zdhf.setText("服务开启");
+                if(!zdhfmessage) {
+                    zdhfmessage=true;
+                    zdhf.setText("回复开启");
                     zdhf.setTextColor(Color.parseColor("#33FF33"));
+                    Toast.makeText(zidonghuihu.this, "回复开启", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                if(hf){
-                    hf=false;
+                if(zdhfmessage){
+                    zdhfmessage=false;
                     zdhf.setText("回复关闭");
                     zdhf.setTextColor(Color.parseColor("#999999"));
+                    Toast.makeText(zidonghuihu.this, "回复关闭", Toast.LENGTH_SHORT).show();
+                    return;
                 }
             }
         });
