3da811ca75c6ca5c8f1450f9d6eaa7c0db850793
061103 <109753786@qq.com>
2019-11-25 19:19:44 +0800

    BIYONG

--- app/src/main/java/com/zhou/biyongxposed/MyDialog.java
+++ app/src/main/java/com/zhou/biyongxposed/MyDialog.java
@@ -9,18 +9,13 @@ import android.view.WindowManager;
 
 class MyDialog extends Dialog {
     //    style引用style样式
-    MyDialog(Context context, int width, int height, View layout, int style) {
-
+    MyDialog(Context context, int width, int height, final View layout, int style) {
         super(context, style);
-
         setContentView(layout);
-
         Window window = getWindow();
-
         WindowManager.LayoutParams params = window.getAttributes();
-
         params.gravity = Gravity.CENTER;
-
         window.setAttributes(params);
     }
+
 }
