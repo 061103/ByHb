3573f2c7e3f30039bde5213f2e1f9bbc3d15e819
061103 <109753786@qq.com>
2019-12-15 16:09:08 +0800

    BIYONG

--- app/src/main/java/com/zhou/biyongxposed/BiyongServer.java
+++ app/src/main/java/com/zhou/biyongxposed/BiyongServer.java
@@ -1,31 +1,38 @@
 package com.zhou.biyongxposed;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static com.zhou.biyongxposed.MainActivity.server_status_check;
 
 
 public class BiyongServer extends Service {
     private static final String TAG = "biyongService";
     private Handler handler = new Handler();
     private boolean run;
+
     @Override
     public void onCreate(){
         super.onCreate();
-        run=false;
-        handler.postDelayed(task, 100);//每秒刷新线程
+        run=true;
+        handler.postDelayed(task, 1000);//每秒刷新线程
         Log.d(TAG,"onCreate executed");
     }
     @Override
     public int onStartCommand(Intent intent,int flags,int startId ){
         Log.d(TAG,"onStartCommand executed");
-        new Thread(new Runnable() {
-            @Override
-              public void run() {
-            }
-          }).start();
         return super.onStartCommand(intent,flags,startId);
     }
     @Override
@@ -41,8 +48,43 @@ public class BiyongServer extends Service {
         @Override
         public void run() {
             if (run) {
-                handler.postDelayed(this, 100);
+                if(server_status_check) {
+                    if(getHigherPackageName().equals("org.telegram.btcchat")){
+                    }
+                }
+                handler.postDelayed(this, 1000);
             }
         }
     };
+
+    /**
+     * 高版本：获取顶层的activity的包名
+     *
+     * @ return
+     */
+    private String getHigherPackageName() {
+        String packagename = "";
+        if(Build.VERSION.SDK_INT >= 22) {
+            UsageStatsManager usage = (UsageStatsManager)getSystemService(Context.USAGE_STATS_SERVICE);
+            long time = System.currentTimeMillis();
+            List<UsageStats> stats = usage.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 10, time);
+            if (stats != null) {
+                SortedMap<Long, UsageStats> runningTask = new TreeMap<Long,UsageStats>();
+                for (UsageStats usageStats : stats) {
+                    runningTask.put(usageStats.getLastTimeUsed(), usageStats);
+                }
+                if (!runningTask.isEmpty()) {
+                    packagename =  runningTask.get(runningTask.lastKey()).getPackageName();
+                }
+            }
+        } else {// if sdk <= 20, can use getRunningTasks
+            ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            //4.获取正在开启应用的任务栈
+            List<ActivityManager.RunningTaskInfo> runningTasks = am.getRunningTasks(1);
+            ActivityManager.RunningTaskInfo runningTaskInfo = runningTasks.get(0);
+            //5.获取栈顶的activity,然后在获取此activity所在应用的包名
+            packagename = runningTaskInfo.topActivity.getPackageName();
+        }
+        return packagename;
+    }
 }
--- app/src/main/java/com/zhou/biyongxposed/GuideActivity.java
+++ app/src/main/java/com/zhou/biyongxposed/GuideActivity.java
@@ -1,25 +1,40 @@
 package com.zhou.biyongxposed;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class GuideActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guide);
+        setContentView(R.layout.activity_fullscreen);
         Window window = getWindow();
-        window.setGravity(Gravity.LEFT | Gravity.TOP);
-        WindowManager.LayoutParams params = window.getAttributes();//获取窗口属性
+        window.setGravity(Gravity.CENTER);
+        WindowManager.LayoutParams params = window.getAttributes();
         params.x = 0;
         params.y = 0;
+        //设置效果为背景透明.
+        params.format = PixelFormat.TRANSLUCENT;
         params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-        params.height = ScreenUtil.dip2px(this,80);
+        //设置窗口宽高
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
         params.width = WindowManager.LayoutParams.MATCH_PARENT;
         window.setAttributes(params);
+        TextView ms_message = findViewById(R.id.messaeg_ms);
+        ms_message.setText("红包任务正在执行");
+    }
+    public void start(Activity act) {
+        Intent intent = new Intent(act, GuideActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        act.startActivity(intent);
     }
 }
